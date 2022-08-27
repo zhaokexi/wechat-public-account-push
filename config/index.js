ef4ec3446d2bf1bee68615f1939e8ebbff429b68
zhaokexi <112251734+zhaokexi@users.noreply.github.com>
2022-08-27 22:23:09 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -29,7 +29,6 @@ export const config = {
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
 // 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是我们结婚的第{{marry_day.DATA}}天
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  

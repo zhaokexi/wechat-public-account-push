176802fb9b5b05639623ca11d408f4eb923d6488
zhaokexi <112251734+zhaokexi@users.noreply.github.com>
2022-08-27 22:32:49 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -16,11 +16,13 @@ export const config = {
     city: "古县",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "老婆", "year": "2004", "date": "08-15", "type": 'r'},
-      {"name": "老公", "year": "2003", "date": "03-29", "type": 'r'},
+      {"name": "任烟", "year": "2004", "date": "08-15", "type": 'r'},
+      {"name": "赵岩", "year": "2003", "date": "03-29", "type": 'r'},
     ],
     // 在一起的日子，格式同上
     loveDate: "2021-10-01",
+    // 结婚纪念日
+    marryDate: "2021-10-01"
     }
 
 // {{date.DATA}}  
@@ -29,6 +31,7 @@ export const config = {
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
 // 今天是我们恋爱的第{{love_day.DATA}}天
+// 今天是我们结婚的第{{marry_day.DATA}}天
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  

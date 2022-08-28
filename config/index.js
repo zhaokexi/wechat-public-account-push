3ebd0ad8b0769188704dfb0e6699325897d44ffc
zhaokexi <112251734+zhaokexi@users.noreply.github.com>
2022-08-28 10:38:54 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ export const config = {
     // 模板消息id
     templateId: "fb_bOcr0OzJsQdC2Aw9vmoohUFBNyoiWYwEnPjg-Q5Y",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: ["of6NN5gEs3VhAlCpVZiFwNaAOBc4","of6NN5nkyCmrcknBQ4nSoJ2ma47s"],
+    user: ["of6NN5gEs3VhAlCpVZiFwNaAOBc4", "of6NN5nkyCmrcknBQ4nSoJ2ma47s"],
      
     // 信息配置
     // 所在省份
@@ -16,8 +16,8 @@ export const config = {
     city: "临汾",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "悦悦", "year": "2004", "date": "08-15", "type": 'r'},
-      {"name": "小赵", "year": "2003", "date": "03-29", "type": 'r'},
+      {"name": "悦悦", "year": "2004", "date": "09-10", "type": 'new'},
+      {"name": "赵岩", "year": "2003", "date": "03-29", "type": 'new'},
     ],
     // 在一起的日子，格式同上
     loveDate: "2021-10-01",
@@ -29,7 +29,9 @@ export const config = {
 // 最低气温: {{min_temperature.DATA}}  
 // 最高气温: {{max_temperature.DATA}}  
 // 今天是我们恋爱的第{{love_day.DATA}}天
+// 今天是我们结婚的第{{marry_day.DATA}}天
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
 // {{note_ch.DATA}}
+Footer
